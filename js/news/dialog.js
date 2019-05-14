1258f938bd523b390713a3c72bdb2cb0412ce1bc
ICEY-I-SEE <1141839113@qq.com>
2019-05-14 14:03:50 +0800

    :art:群聊

--- js/news/dialog.js
+++ js/news/dialog.js
@@ -1,6 +1,10 @@
 $(function(){
     //解决iOS fixed定位失效
-    $('.dialog_menu_input').focus(function(){
-        
+    $('.dialog_menu_input').keyup(function(){
+        if($(this).val()!=''){
+            $('.dialog_menu_send').css('background','#128ae6')
+        }else{
+            $('.dialog_menu_send').css('background','#c3f4ff')
+        }
     })
 })
