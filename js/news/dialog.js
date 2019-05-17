c0b2098932d85abf0152be11e613e988823354ef
zhangyunfei <86975943@qq.com>
2019-05-17 15:46:28 +0800

    表情

--- js/news/dialog.js
+++ js/news/dialog.js
@@ -74,4 +74,18 @@ $(function(){
     $('.dialog_transfer_back').click(function(){
         $('.dialog_transfer_wrap').hide()
     })
+    //点击表情包的图片，让滚动条滚动到底部
+    $('.emotion_ear').click(function(){
+    	
+//  	console.log($(document).scrollTop())
+//  	$("html,body").animate({scrollTop:$(document).scrollTop()},1000);
+    	
+    	
+    })  
+   //图片绑定表情包
+   $('.emotion_ear').SinaEmotion($('.dialog_menu_input'));
+// $('#face').SinaEmotion($('.emotion'));
+   //测试本地解析
+   
+	
 })
